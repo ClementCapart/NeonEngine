f0bff3c65caf9047da087cbb6e0b587af9dd98bd
Clement Capart <clement.capart@gmail.com>
2013-09-20 21:03:42 +0200

    NeonStarInput Enum to list needed inputs

---
+++ NeonStarLibrary/NeonStarInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeonStarLibrary
+{
+    public enum NeonStarInput
+    {
+        MoveLeft,
+        MoveRight,
+        MoveUp,
+        MoveDown,
+        Jump,
+        Attack,
+        UseElement,
+        SwitchElements,
+        MixElements,
+        DropElement,
+    }
+}
