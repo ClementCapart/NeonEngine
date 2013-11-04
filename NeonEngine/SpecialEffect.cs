9341cc6960c16927a55c0be2a0e7fc176c5a3d6e
Clement <clement.capart@gmail.com>
2013-11-04 15:02:25 +0100

    SpecialEffect.cs

---
+++ NeonEngine/SpecialEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeonEngine
+{
+    public class AnimatedSpecialEffect : Entity
+    {
+        public AnimatedSpecialEffect(World containerWorld)
+            :base(containerWorld)
+        {
+
+        }
+    }
+}
