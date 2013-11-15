1d32d63d7a85b7962517e79659039975d4af78bb
Solinac <nja.salvy@gmail.com>
2013-11-15 01:42:08 +0100

    Scripts

---
+++ NeonStar/NeonStar/out/Data/Scripts/DoorClosing.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NeonEngine;
+using NeonStarLibrary;
+using NeonEngine.Private;
+using Microsoft.Xna.Framework;
+
+namespace NeonScripts
+{
+    public class DoorClosingScript : ScriptComponent
+    {
+        private string _triggerName = "";
+
+        public string TriggerName
+        {
+            get { return _triggerName; }
+            set { _triggerName = value; }
+        }
+
+        private string _closingDoorAnimationName = "";
+
+        public string ClosingDoorAnimationName
+        {
+            get { return _closingDoorAnimationName; }
+            set { _closingDoorAnimationName = value; }
+        }
+
+        public DoorClosingScript(Entity entity)
+            :base(entity, "DoorClosingScript")
+        {
+        }
+		
+		public override void Init()
+		{
+            entity.spritesheets.ChangeAnimation(_closingDoorAnimationName, 0, false, true, false, 9);
+		}
+
+        public override void Update(GameTime gameTime)
+        {
+            if (entity.spritesheets.CurrentSpritesheet.currentFrame == 0)
+                entity.spritesheets.CurrentSpritesheet.isPlaying = false;
+        }
+		
+		public override void OnTrigger(Entity trigger, Entity triggeringEntity)
+		{
+            if (trigger.Name == _triggerName)
+            {
+                entity.spritesheets.ChangeAnimation(_closingDoorAnimationName, 0, true, true, true, entity.spritesheets.SpritesheetList[_closingDoorAnimationName].FrameCount - 1);
+                entity.spritesheets.CurrentSpritesheet.ReverseLoop = true;
+                entity.rigidbody.IsGround = true;
+                entity.rigidbody.Init();
+            }
+		}
+    }
+}
---
+++ NeonStar/NeonStar/out/Data/Scripts/FireDoorScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NeonEngine;
+using NeonStarLibrary;
+using NeonEngine.Private;
+using Microsoft.Xna.Framework;
+
+namespace NeonScripts
+{
+    public class FireDoorScript : ScriptComponent
+    {
+        public FireDoorScript(Entity entity)
+            :base(entity, "FireDoorScript")
+        {
+        }
+		
+		public override void Init()
+		{
+            //if (entity.spritesheets.Name == "FireDoorOpening")
+                entity.spritesheets.ChangeAnimation("FireDoorOpening", 0, false, true, false, 0);
+		}
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+		
+		public override void OnTrigger(Entity trigger, Entity triggeringEntity)
+		{
+		}
+    }
+}
