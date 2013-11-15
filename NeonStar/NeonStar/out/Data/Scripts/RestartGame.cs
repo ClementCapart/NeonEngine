de05b139bb776590dd235ad7c681c4d57e69cd79
Solinac <nja.salvy@gmail.com>
2013-11-15 17:13:21 +0100

    many things

---
+++ NeonStar/NeonStar/out/Data/Scripts/RestartGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NeonEngine.Private;
+using NeonEngine;
+using NeonStarLibrary;
+
+namespace NeonStarScripts
+{
+    class RestartGame : ScriptComponent
+    {
+        public RestartGame(Entity entity)
+            : base(entity, "RestartGame")
+        {
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            if (Neon.Input.Pressed(Microsoft.Xna.Framework.Input.Buttons.Back))
+                Neon.world.ChangeScreen(new GameScreen(Neon.game));
+            base.Update(gameTime);
+        }
+    }
+}
