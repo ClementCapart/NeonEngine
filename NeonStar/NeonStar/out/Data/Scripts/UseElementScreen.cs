a30bb4e87d16a2faf0e4195dbb3753de5d41abfd
Solinac <nja.salvy@gmail.com>
2013-11-19 17:13:32 +0100

    UseElementScreenScript

---
+++ NeonStar/NeonStar/out/Data/Scripts/UseElementScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NeonEngine;
+using NeonStarLibrary;
+using NeonEngine.Private;
+using Microsoft.Xna.Framework;
+
+namespace NeonScripts
+{
+    public class UseElementScreenScript : ScriptComponent
+    {
+        private string _avatarName = "";
+
+        public string AvatarName
+        {
+            get { return _avatarName; }
+            set { _avatarName = value; }
+        }
+
+        private Avatar _avatar = null;
+
+        public UseElementScreenScript(Entity entity)
+            : base(entity, "UseElementScreenScript")
+        {
+        }
+		
+		public override void Init()
+		{
+            if (_avatarName != "")
+                _avatar = Neon.world.GetEntityByName(_avatarName).GetComponent<Avatar>();
+			entity.spritesheets.ChangeAnimation("Off");
+		}
+
+        public override void Update(GameTime gameTime)
+        {
+            if (_avatar != null)
+                if (_avatar.elementSystem.LeftSlotElement == Element.Fire)
+				{
+                    entity.graphic.Remove();
+                    entity.spritesheets.ChangeAnimation("UseElement");
+				}
+        }
+		
+		public override void OnTrigger(Entity trigger, Entity triggeringEntity, object[] parameters = null)
+		{
+		}
+    }
+}
