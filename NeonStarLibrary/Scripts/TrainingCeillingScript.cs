88cc1f7b764b9b93a34a1cd73a010f2febe06daf
Solinac <nja.salvy@gmail.com>
2014-03-04 09:54:33 +0100

    Ceillingscript

---
+++ NeonStarLibrary/Scripts/TrainingCeillingScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NeonEngine;
+using NeonStarLibrary;
+using NeonEngine.Private;
+using Microsoft.Xna.Framework;
+using NeonStarLibrary.Components.Avatar;
+
+namespace NeonStarLibrary.Components.Scripts
+{
+    public class TrainingCeillingScript : ScriptComponent
+    {
+        private Entity _flyingBotIn;
+        private Entity _flyingBotOut;
+
+        public TrainingCeillingScript(Entity entity)
+            :base(entity, "TrainingCeillingScript")
+        {
+        }
+		
+		public override void Init()
+		{
+            _flyingBotIn = entity.GameWorld.GetEntityByName("FlyingBotIn");
+            _flyingBotOut = entity.GameWorld.GetEntityByName("FlyingBotOut");
+		}
+
+        public override void Update(GameTime gameTime)
+        {
+            if(_flyingBotIn != null && _flyingBotIn.transform.Position.X <= -1500)
+            {
+                _flyingBotIn.transform.Position += new Vector2(3200,0);
+            }
+            if (_flyingBotOut != null && _flyingBotOut.transform.Position.X >= 1500)
+            {
+                _flyingBotOut.transform.Position += new Vector2(-3200, 0);
+            }
+        }
+    }
+}
