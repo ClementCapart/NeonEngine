b3fa6259c9540c43d3b345fbca1601bd28d85aea
Solinac <nja.salvy@gmail.com>
2014-03-04 12:42:36 +0100

    TrainingEntranceScript

---
+++ NeonStarLibrary/Scripts/TrainingEntranceScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NeonEngine;
+using NeonStarLibrary;
+using NeonEngine.Private;
+using Microsoft.Xna.Framework;
+using NeonStarLibrary.Components.Avatar;
+
+namespace NeonStarLibrary.Components.Scripts
+{
+    public class TrainingEntranceScript : ScriptComponent
+    {
+        private Entity _airWave;
+
+        public TrainingEntranceScript(Entity entity)
+            :base(entity, "TrainingEntranceScript")
+        {
+        }
+		
+		public override void Init()
+		{
+            _airWave = entity.GameWorld.GetEntityByName("AirWave");
+		}
+
+        public override void Update(GameTime gameTime)
+        {
+            if(_airWave != null && _airWave.transform.Position.X <= 0)
+            {
+                _airWave.transform.Position += new Vector2(1500,0);
+            }
+        }
+    }
+}
